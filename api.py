5903afe1a7d38e946c92f67209e2605840f64b39
Pranav <Nagapran@ualberta.ca>
2026-06-02 18:56:59 -0600

    initial commit - NHL shot predictor API

--- api.py
+++ api.py
@@ -55,6 +55,44 @@ async def search(query: str):
                 })
         return players[:5]
 
+@app.get("/teams")
+def teams():
+    return [
+        {"abbrev": "ANA", "name": "Anaheim Ducks", "logo": "https://assets.nhle.com/logos/nhl/svg/ANA_light.svg"},
+        {"abbrev": "BOS", "name": "Boston Bruins", "logo": "https://assets.nhle.com/logos/nhl/svg/BOS_light.svg"},
+        {"abbrev": "BUF", "name": "Buffalo Sabres", "logo": "https://assets.nhle.com/logos/nhl/svg/BUF_light.svg"},
+        {"abbrev": "CGY", "name": "Calgary Flames", "logo": "https://assets.nhle.com/logos/nhl/svg/CGY_light.svg"},
+        {"abbrev": "CAR", "name": "Carolina Hurricanes", "logo": "https://assets.nhle.com/logos/nhl/svg/CAR_light.svg"},
+        {"abbrev": "CHI", "name": "Chicago Blackhawks", "logo": "https://assets.nhle.com/logos/nhl/svg/CHI_light.svg"},
+        {"abbrev": "COL", "name": "Colorado Avalanche", "logo": "https://assets.nhle.com/logos/nhl/svg/COL_light.svg"},
+        {"abbrev": "CBJ", "name": "Columbus Blue Jackets", "logo": "https://assets.nhle.com/logos/nhl/svg/CBJ_light.svg"},
+        {"abbrev": "DAL", "name": "Dallas Stars", "logo": "https://assets.nhle.com/logos/nhl/svg/DAL_light.svg"},
+        {"abbrev": "DET", "name": "Detroit Red Wings", "logo": "https://assets.nhle.com/logos/nhl/svg/DET_light.svg"},
+        {"abbrev": "EDM", "name": "Edmonton Oilers", "logo": "https://assets.nhle.com/logos/nhl/svg/EDM_light.svg"},
+        {"abbrev": "FLA", "name": "Florida Panthers", "logo": "https://assets.nhle.com/logos/nhl/svg/FLA_light.svg"},
+        {"abbrev": "LAK", "name": "Los Angeles Kings", "logo": "https://assets.nhle.com/logos/nhl/svg/LAK_light.svg"},
+        {"abbrev": "MIN", "name": "Minnesota Wild", "logo": "https://assets.nhle.com/logos/nhl/svg/MIN_light.svg"},
+        {"abbrev": "MTL", "name": "Montreal Canadiens", "logo": "https://assets.nhle.com/logos/nhl/svg/MTL_light.svg"},
+        {"abbrev": "NSH", "name": "Nashville Predators", "logo": "https://assets.nhle.com/logos/nhl/svg/NSH_light.svg"},
+        {"abbrev": "NJD", "name": "New Jersey Devils", "logo": "https://assets.nhle.com/logos/nhl/svg/NJD_light.svg"},
+        {"abbrev": "NYI", "name": "New York Islanders", "logo": "https://assets.nhle.com/logos/nhl/svg/NYI_light.svg"},
+        {"abbrev": "NYR", "name": "New York Rangers", "logo": "https://assets.nhle.com/logos/nhl/svg/NYR_light.svg"},
+        {"abbrev": "OTT", "name": "Ottawa Senators", "logo": "https://assets.nhle.com/logos/nhl/svg/OTT_light.svg"},
+        {"abbrev": "PHI", "name": "Philadelphia Flyers", "logo": "https://assets.nhle.com/logos/nhl/svg/PHI_light.svg"},
+        {"abbrev": "PIT", "name": "Pittsburgh Penguins", "logo": "https://assets.nhle.com/logos/nhl/svg/PIT_light.svg"},
+        {"abbrev": "STL", "name": "St. Louis Blues", "logo": "https://assets.nhle.com/logos/nhl/svg/STL_light.svg"},
+        {"abbrev": "SJS", "name": "San Jose Sharks", "logo": "https://assets.nhle.com/logos/nhl/svg/SJS_light.svg"},
+        {"abbrev": "SEA", "name": "Seattle Kraken", "logo": "https://assets.nhle.com/logos/nhl/svg/SEA_light.svg"},
+        {"abbrev": "TBL", "name": "Tampa Bay Lightning", "logo": "https://assets.nhle.com/logos/nhl/svg/TBL_light.svg"},
+        {"abbrev": "TOR", "name": "Toronto Maple Leafs", "logo": "https://assets.nhle.com/logos/nhl/svg/TOR_light.svg"},
+        {"abbrev": "UTA", "name": "Utah Hockey Club", "logo": "https://assets.nhle.com/logos/nhl/svg/UTA_light.svg"},
+        {"abbrev": "VAN", "name": "Vancouver Canucks", "logo": "https://assets.nhle.com/logos/nhl/svg/VAN_light.svg"},
+        {"abbrev": "VGK", "name": "Vegas Golden Knights", "logo": "https://assets.nhle.com/logos/nhl/svg/VGK_light.svg"},
+        {"abbrev": "WSH", "name": "Washington Capitals", "logo": "https://assets.nhle.com/logos/nhl/svg/WSH_light.svg"},
+        {"abbrev": "WPG", "name": "Winnipeg Jets", "logo": "https://assets.nhle.com/logos/nhl/svg/WPG_light.svg"}
+    ]
+
+
 @app.get("/history/{player_id}")
 def history(player_id: int):
     conn = sqlite3.connect("nhl.db")
